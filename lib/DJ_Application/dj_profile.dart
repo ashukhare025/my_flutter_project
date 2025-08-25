46036c85a2f082cc18743379895314d598d6cdbb
Ashu Khare <ashukhare1994@gmail.com>
2025-08-25 17:45:34 +0530

    newupdate

--- lib/DJ_Application/dj_profile.dart
+++ lib/DJ_Application/dj_profile.dart
@@ -6,7 +6,7 @@ class DjProfile extends StatelessWidget {
   const DjProfile({super.key});
 
   @override
-  Widget build(BuildContext context) {g
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
@@ -16,7 +16,6 @@ class DjProfile extends StatelessWidget {
             filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
             child: Container(color: Colors.black.withOpacity(0)),
           ),
-
           Container(
             height: double.infinity,
             width: double.infinity,
@@ -29,6 +28,141 @@ class DjProfile extends StatelessWidget {
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
               ),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: 100),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 120,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage('assets/DJ/gamer.png'),
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      right: 10,
+                      child: Container(
+                        padding: EdgeInsets.all(6),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Icon(
+                          Icons.camera_alt_outlined,
+                          size: 40,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 50),
+                Column(
+                  children: [
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFFFFFFFF),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          hintText: 'City',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFFFFFFFF),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          hintText: 'E-mail',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFFFFFFFF),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: TextField(
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          hintText: 'Phone',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFFFFFFFF),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 25),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 130,
+                          vertical: 15,
+                        ),
+                        backgroundColor: Colors.blue.shade900,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(34),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        'Update Profile',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontFamily: 'FontMain',
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Change password',
+                            style: TextStyle(
+                              color: Color(0XFFFFFFFF),
+                              fontWeight: FontWeight.w400,
+                              fontFamily: 'FontMain',
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
